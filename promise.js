ad80b817b0b7c5103dcd6f9da7aa5b4473606ae2
purwo hadi <purwo_ok@yahoo.com>
2022-06-10 10:37:15 +0700

    perubahan assesment Tantangan: Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null;
+const promiseOutput = async (emosi) => {  
+    const promiseIXX = await promiseTheaterIXX().then((dataValue)=>{ return dataValue}).catch((error)=>{return []})
+    const promiseVGC = await promiseTheaterVGC().then((dataValue)=>{ return dataValue}).catch((error)=>{return []})
+
+	const promiseTheater = [...promiseIXX, ...promiseVGC]
+	const hasil = promiseTheater.filter(element =>(element.hasil === emosi)).length 
+	return hasil
+};
 
 module.exports = {
   promiseOutput,
